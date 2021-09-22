364c00037b05888af3a74e07c3fc1458930b8a88
Matthew <Firerexman007@gmail.com>
2021-09-22 16:25:10 +0000

    Program 2 outline

---
+++ Program 2 Vigenere Cipher/VigenereProgram.py
@@ -0,0 +1,25 @@
+# Pragram 2 Vigenere Cipher 
+# Team: Hermit 2021 Fall Quarter 
+# Class: CSC 442-001
+
+########### global Variables 
+inputs =[] #keeps all inputs 
+outputs =[] #keeps all outputs 
+
+########### functions
+
+#Decode 
+
+#EnCode
+
+
+########### code 
+#Start code and determin if encoding or decoding 
+
+#loop
+    # listen for input 
+        #place in inputs
+            #de/en-code
+
+#end of inputs
+    #print outputs 
