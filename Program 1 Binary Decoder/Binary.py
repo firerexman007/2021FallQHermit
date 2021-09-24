677f2255b43db20c34374676a766119d18e4b896
Cori Allbritton <therealcori1@gmail.com>
2021-09-24 17:39:48 -0500

    Uploading finished code

--- Program 1 Binary Decoder/Binary.py
+++ Program 1 Binary Decoder/Binary.py
@@ -1,39 +1,87 @@
 ################################################################
 #Group: Hermit sub-team A
-#Names: Cori Allbritton, Varat Chaichamnarn, Thien Vu 
+#Names: Cori Allbritton, Varat Chaichamnarn, Thien Vu
 #Desc: Decodes binary into 7-bit or 8-bit ASCII and prints it
 #Date: 9-21-2021
 ################################################################
 #Division of labor:
-#Cori: Input files
-#Varat: Decoding ASCII
+#Cori: File merging
+#Varat: Decoding ASCII and writeup
 #Thien: Differentiation of 7-bit and 8-bit ASCII
 #Wheel of names when done: Writeup
-#Update: Cori's Python is messed up,
-#        so she'll write the report instead.
-#        Thien takes over file input. 
 
 import fileinput
 
+
 #detects whether its the ASCII is in 7bit or 8bit
 def detectorASCII(binaryASCII):
-    if (len(binaryASCII) % 7 == 0 and len(binaryASCII) % 8 == 0):
-        #put Varat's code here
-        print("7and8")
-    elif(len(binaryASCII) % 7 == 0):
-        #put Varat's code here
-        print("7")
-    elif(len(binaryASCII) % 8 == 0):
-        #put Varat's code here
-        print("8")
-    else:
-        print("Does not match 7-bit or 8-bit ASCII values, aborting.")
+    finalStr = ""
+    if(len(binaryASCII) % 7 == 0):
+        print("7-Bit Conversion")
 
+        # slicing the input and converting it
+        # in decimal and then converting it in string
+        for i in range(0, len(binaryASCII), 7):
+
+            # slicing the bin_data from index range [0, 6]
+            # and storing it as integer in temp_data
+            temp_data = int(binaryASCII[i:i + 7])
+
+            if (temp_data == 1000):
+                finalStr = finalStr.rstrip(finalStr[-1])
+
+            else:
+                # passing temp_data in BinarytoDecimal() function
+                # to get decimal value of corresponding temp_data
+                decimal_data = BinaryToDecimal(temp_data)
+
+                # Deccoding the decimal value returned by
+                # BinarytoDecimal() function, using chr()
+                # function which return the string corresponding
+                # character for given ASCII value, and store it
+                # in str_data
+                finalStr = finalStr + chr(decimal_data)
+        print(finalStr)
+
+    if(len(binaryASCII) % 8 == 0):
+        print("8-Bit Conversion")
+
+        # slicing the input and converting it
+        # in decimal and then converting it in string
+        for i in range(0, len(binaryASCII), 8):
+
+            # slicing the binaryASCII from index range [0, 7]
+            # and storing it as integer in temp_data
+            temp_data = int(binaryASCII[i:i + 8])
+
+            if (temp_data == 1000):
+                finalStr = finalStr.rstrip(finalStr[-1])
+
+            else:
+                # passing temp_data in BinarytoDecimal() function
+                # to get decimal value of corresponding temp_data
+                decimal_data = BinaryToDecimal(temp_data)
+
+                # Deccoding the decimal value returned by
+                # BinarytoDecimal() function, using chr()
+                # function which return the string corresponding
+                # character for given ASCII value, and store it
+                # in str_data
+                finalStr = finalStr + chr(decimal_data)
+        print(finalStr)
+# Defining BinarytoDecimal() function
+def BinaryToDecimal(binary):
+    binary1 = binary
+    decimal, i, n = 0, 0, 0
+    while (binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary // 10
+        i += 1
+    return (decimal)
 #testing part
 binaryASCII = ""
 #file will be input like "CSC442BinaryDecoder.py < [some file]"
 for line in fileinput.input():
     binaryASCII += line.rstrip()
-print(binaryASCII)
-print(len(binaryASCII))
 detectorASCII(binaryASCII)
