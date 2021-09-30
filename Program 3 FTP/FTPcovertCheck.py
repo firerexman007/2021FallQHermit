67b4bcee1f44a7f892f74e2b25308d4890ac16bf
colbyboyd410 <56280845+colbyboyd410@users.noreply.github.com>
2021-09-30 17:07:00 -0500

    Update FTPcovertCheck.py
    added binary to ASCII changes

--- Program 3 FTP/FTPcovertCheck.py
+++ Program 3 FTP/FTPcovertCheck.py
@@ -9,6 +9,24 @@ def covertCheck(permBits):
     #covert
     return True
 
+#from binary numbers to ASCII to characters
+def covertMessage(binary, n):
+    text = ""
+    i = 0
+    while (i < len(binary)):
+        #slicing the  binary numbers from index range [0-n]
+        bits = binary[i:i+n]
+        bits = int(bits, 2)
+        #remove last character of string if backspace
+        if(bits == 7):
+            text = text[:-1]
+            i+=n
+        else:
+            #character for given ASCII value
+            text += chr(bits)
+            i += n
+    return text
+
 #testing
 #permissions go here
 permBits = "1111100000"
