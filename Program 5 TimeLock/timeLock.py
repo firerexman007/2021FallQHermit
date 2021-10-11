be6d5261d6f439e98bd9673b0735eb6e97a40a3a
MatthewT <Firerexman007@gmail.com>
2021-10-11 11:50:24 -0500

    Program for timelock.py started

---
+++ Program 5 TimeLock/timeLock.py
@@ -0,0 +1,72 @@
+################################################################
+#Group: Hermit 
+#Names: Matthew Tucker, 
+#Desc: 
+#Date: 10/11/2021
+################################################################
+#Division of labor:
+#Matthew: Input/output system
+#ADD YOUR NAME 
+# All members worked together and help when others strugled
+
+
+## HAVE NOT TESTED YET JUST DID QUICKLY IN CLASS
+
+#imports 
+import sys
+import re
+from datetime import datetime 
+from hashlib import md5
+import pytz
+
+# sets 
+INTERVAL = 60
+MANUAL_DATETIME =""
+
+
+#functions 
+def getTime(Y,M,D,H,m,S):
+    seconds = 0
+    M += (Y*12)
+    D += (M*30)
+    H += (D*24)
+    m += (H*60)
+    s += (m*60)
+    seconds = s
+    return seconds 
+    
+
+def getCode(time1):
+    if(MANUAL_DATETIME == ""):
+        currentTime = ""
+    else:
+        currentTime = MANUAL_DATETIME
+    print("Current(UTC) " +sys.argv[1] +" "+ sys.argv[2] +" "+ sys.argv[3] +" "+ sys.argv[4] +" "+ sys.argv[5] +" "+ sys.argv[6] # test code 
+    print("Epoch (UTC )" + MANUAL_DATETIME) # test code  
+    
+    time2 = getTime(currentTime)
+    timediff = time1-(time2%60)
+    print("Seconds: " + time1) # test code 
+    print("Seconds: " + time2) # test code 
+    string = md5(timediff)
+    print("MD5 #1: " + string) # test code 
+    string2 = md5(string)
+    print("MD5 #2: " + string2) # test code 
+    
+    #get the final bit
+    
+    
+
+
+
+###########Start of Program
+
+if len(sys.argv)>=6: # for user input handling 
+    # change input time to seconds 
+    time1 = getTime(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+    print(time1) # test seconds
+    stringF = getCode(time1)
+    print(stringF)
+else:
+    print("Incorrect input")
+    print("python3 " + sys.argv[0] + "YYYY MM DD HH mm SS")
