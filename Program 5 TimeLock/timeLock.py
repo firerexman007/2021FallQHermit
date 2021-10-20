2773e7dc5bf066106c81b98416cbcbd946cdf5f9
Matthew T <Firerexman007@gmail.com>
2021-10-20 17:39:01 -0500

    Timelock and epoch

--- Program 5 TimeLock/timeLock.py
+++ Program 5 TimeLock/timeLock.py
@@ -17,61 +17,35 @@ import sys
 import re
 from datetime import datetime, date 
 import calendar 
-from hashlib import md5
+import hashlib 
 
 #import pytz
 
 # sets 
 INTERVAL = 60
-#MANUAL_DATETIME = "2021 2 23 0 0 0"
-MANUAL_DATETIME = ""
+#current time
+MANUAL_DATETIME = "2013 05 06 07 43 25"
+#MANUAL_DATETIME = ""
 
 #functions 
     # convert YYYY, MM, DD, HH, mm, S to seconds 
 def getTime(Y,M,D,H,m,S):
     seconds = 0
-    #print(Y)
-    M += (Y*12)
-    #print(M)
-    D += (M*30)
-    H += (D*24)
-    m += (H*60)
-    S += (m*60)
-    seconds = S
+    seconds += (Y*31556926)
+    seconds += (M*2629743)
+    seconds += (D*86400)
+    seconds += (H*3600)
+    seconds += (m*60)
+    seconds += S
+    print(seconds)
     return seconds 
-    
-    # convert everything to final string 
-def getCode(time1):
-    # if not manual datetime then get current date time 
-    if(MANUAL_DATETIME == ""):
-        currentTime = "" # get time stamp from computer 
-    else:
-        currentTime = MANUAL_DATETIME
-    # print out for test 
-    print("Current(UTC) ") # test code 
-    print("Epoch (UTC ) " + MANUAL_DATETIME) # test code 
-    
-    # get time diff
-    time2 = getTime(currentTime)
-    timediff = time1-(time2%60)
-    #print test 
-    print("Seconds: " + time1) # test code 
-    print("Seconds: " + time2) # test code 
-    
-    # get md5 and print test 
-    string = md5(timediff)
-    print("MD5 #1: " + string) # test code 
-    string2 = md5(string)
-    print("MD5 #2: " + string2) # test code 
-    
-    #get the final bit
-    
+     
     
 
 
 
 ###########Start of Program
-if(not(sys.stdin.isatty())): # if there is input from start of run
+if((not(sys.stdin.isatty()))): # if there is input from start of run
     # get input from .txt or echo
     year = sys.stdin.readline().rstrip()
     #print("{}".format(year)) # test print
@@ -89,40 +63,52 @@ if(not(sys.stdin.isatty())): # if there is input from start of run
     cE = year.split( )
 
     # convert to seconds 
+    print(cT)
+    print(cE)
     timeN = getTime(int(cT[0]),int(cT[1]),int(cT[2]),int(cT[3]),int(cT[4]),int(cT[5]))
     timeE = getTime(int(cE[0]),int(cE[1]),int(cE[2]),int(cE[3]),int(cE[4]),int(cE[5]))
-    #print(timeN) # test seconds 
-    #print(timeE)
-
-    # get time diff with only chaning every 60 sec
-    timeD = timeN-timeE - ((timeN-timeE)%60)       ##### CHECK THIS 
-    #print(timeD)
     
+    print(timeN) # test seconds 
+    print(timeE)
+    print("Time diff " + str((timeE%INTERVAL)))
+
+    # get time diff with only chaning every interval sec
+    timeD = int(timeN-timeE)       ##### CHECK THIS                  ## SOMTHING WRONG HERE
+    if(not(int(cE[5])%INTERVAL == 0)):
+        timeF =str(timeD-int(cE[5])%INTERVAL)
+    
+    print(timeF)
+      
+    #timeD = str(421137839)  # HARD TEST CODE 
     # convert to encoded md5
-    string = md5.new((timeD))
+    string = (hashlib.md5((timeF).encode('utf-8')).hexdigest())      ## SOMTHING WRONG HERE
     print("MD5 #1: " + string) # test code 
-    string2 = md5.new((string))
+    string2 = (hashlib.md5((string).encode('utf-8')).hexdigest())
     print("MD5 #2: " + string2) # test code 
     
+    # get output
+    only_alpha ="" # For all letters 
+    only_number = ""  # for all numbers 
+    for char in string2: # go charater by charater
+        # get the ACII num to determin what it is 
+        if(ord(char) >= 65 and ord(char) <= 90):  # lower case 
+            only_alpha += char
+        elif(ord(char) >= 97 and ord(char) <= 122): # upper case 
+            only_alpha += char
+        elif(ord(char) >= 48 and ord(char) <= 57): # numbers 
+            only_number += char
+    key ="" # set up final key 
+    # get first two alphabetical and last to numbers 
+    key += only_alpha[0:2]
+    key += only_number[-1]
+    key += only_number[-2]
+    print(key) # output key END OF CODE
 
-else: # else print error
+else: # else print error for in correct use 
     print("Incorrect input. Please use: ")
     print('echo "YYYY MM DD HH mm SS" | python3 ' + sys.argv[0])
     print("or")
     print('python3 ' + sys.argv[0] + ' < input.txt')
 
     
-"""
-if len(sys.argv)>=6: # for user input handling 
-    # convert to UTC TIME 
-    
 
-    # change input time to seconds 
-    time1 = getTime(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
-    print(time1) # test seconds
-    stringF = getCode(time1)
-    print(stringF)
-else:
-    print("Incorrect input")
-    print("python3 " + sys.argv[0] + "YYYY MM DD HH mm SS")
-"""
