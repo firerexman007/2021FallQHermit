61476985724d81488e7a18ea9b976929afa69d09
Matthew T <firerexman007@gmail.com>
2021-10-26 10:15:29 -0400

    Steg input system done

--- Program 7 Steganography/Steg.py
+++ Program 7 Steganography/Steg.py
@@ -5,7 +5,7 @@
 #Date: 10/25/2021
 ################################################################
 #Division of labor:
-#Matthew: input/output system 
+#Matthew: input/output system, help system
 #Thien: 
 # All members worked together and help when others struggled
 
@@ -14,8 +14,8 @@ import sys
 import re
 
 
-# Start of Code 
-# help mode
+###### Start of Code 
+## help mode
 if(len(sys.argv)>1):
     if(sys.argv[1] == "h" or sys.argv[1] == "help"):
         print("Python3 "+ sys.argv[0] + " -(sr) -(bB) -o<val> [-i<val>] -w<val> [-h<val>]")
@@ -29,44 +29,73 @@ if(len(sys.argv)>1):
         print("-h<val>  set hiddent file to <val>")
         exit(0)
 
-#Check if input is valid 
-if len(sys.argv)>6:
-    #check each set of input in correc order and there 
-    if(sys.argv[1] != "-s" and sys.argv[1] != "-r"):
-        print("Please -s for Store and -r for Retrieve mode")
+## main mode (get/org inputs) 
+Pwrap =""
+if len(sys.argv)>4: # see if have min inputs needed  
+    if(sys.argv[1] == "-s" or sys.argv[1] == "-r"): # if there
+        Pmode = sys.argv[1][1:]
+    else: # else error control 
+        print("Please use -s for Store and -r for Retrieve mode")
         exit(0)
-    if(sys.argv[2] != "-b" and sys.argv[2] != "-B"):
-        print("Please -b for bit and -B for byte mode")
+    if(sys.argv[2] == "-b" or sys.argv[2] == "-B"): # if there
+        Ptype = sys.argv[2][1:]
+    else: # else error control
+        print("Please use -b for bit and -B for byte mode")
         exit(0)
-    if(sys.argv[3][0:2] != "-o"):
-        print("Please -o<val> set offset to <val> (defualt is 0)")
+    if(sys.argv[3][0:2] == "-o"): # if there 
+        PoffS = sys.argv[3][2:]
+        if(PoffS ==""): # if there but no val
+            PoffS = "0"
+    else: # else error control
+        print("Please use -o<val> set offset to <val> (defualt is 0)")
         exit(0)
-    if(sys.argv[4][0:2] != "-i"):
-        print("Please -i<val> set interval to <val> (defualt is 1)")
+    if(sys.argv[4][0:2] == "-i"): # if there 
+        Pinte = sys.argv[4][2:] 
+        if(Pinte == ""): # if there but no val
+           Pinte = "1"
+    elif(sys.argv[4][0:2] == "-w"): # if not there 1 , if there 2
+           Pwrap = sys.argv[4][2:]
+           Pinte ="1"
+    else: # else error control
+        print("Please use -i<val> set interval to <val> (defualt is 1)")
+        print("OR -w<val> set wrapper file to <val>")
         exit(0)
-    if(sys.argv[5][0:2] != "-w"):
-        print("Please -w<val> set wrapper file to <val>")
-        exit(0)
-    if(sys.argv[6][0:2] != "-h"):
-        print("Please -h<val> set hiddent file to <val>")
-        exit(0)
-    Pmode = sys.argv[1][1:]
-    Ptype = sys.argv[2][1:]
-    PoffS = sys.argv[3][2:]
-    Pinte = sys.argv[4][2:]    
-    Pwrap = sys.argv[5][2:]
-    Phidd = sys.argv[6][2:]
-        
+    if(len(sys.argv)>5): # if there are more inputs
+        if(Pwrap == sys.argv[4][2:]): # if Pwrap has been made
+            if(sys.argv[5][0:2] == "-h"): # if there 
+                Phidd = sys.argv[5][2:]
+            else: # default
+                Phidd = ""
+        else: # if not done
+            if(sys.argv[5][0:2] == "-w"): # if there 
+                Pwrap = sys.argv[5][2:] 
+            else: # else error control
+                print("Please use -w<val> set wrapper file to <val>")
+                exit(0)
+            if(len(sys.argv)>6): # if more inputs 
+                if(sys.argv[6][0:2] == "-h"): # if there 
+                    Phidd = sys.argv[6][2:]
+                else: # default
+                    Phidd = ""
+            else: # else if no more inputs 
+                Phidd = ""   
+    else:
+         Phidd =""      
     #test input system
+    """
     print(f"Mode = {Pmode}")
     print(f"Type = {Ptype}") 
     print(f"OffS = {PoffS}") 
     print(f"Inte = {Pinte}") 
     print(f"Wrap = {Pwrap}") 
     print(f"Hidd = {Phidd}") 
-
-else: # invalid system
+    """
+else: # invalid system not enough inputs 
     print("INVALID Input:")
     print("Python3 "+ sys.argv[0] + " -(sr) -(bB) -o<val> [-i<val>] -w<val> [-h<val>]")
     print("Need help run:")
     print("Python3 "+ sys.argv[0] + " h")
+    exit(0)
+
+#The rest of the code with inputs 
+
