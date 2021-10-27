17b41cf11956f7dd6be47d90576f76d5cd8ad19d
Siva Poornima Karuturi <39521423+SivaPKaruturi@users.noreply.github.com>
2021-10-27 11:07:19 -0500

    Add files via upload

---
+++ Program 6 XOR/xor.py
@@ -0,0 +1,8 @@
+from sys import stdin, stdout
+
+key = bytearray(open("key","rb").read())
+#print(key)
+message=bytearray(stdin.buffer.read())
+for i in range(0,len(message)):
+    temp=bytes([message[i]^key[i%len(key)]])
+    stdout.buffer.write(temp)
